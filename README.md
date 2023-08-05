5c7c33fa883aa1e5a5bfb6a3b7deb300b09ae549
phoenix <dias@solailabs.com>
2023-08-05 12:59:20 +0600

    Added CLI and docs

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # tsp_sim
+
+## Installation
+
+- Install [miniforge](https://github.com/conda-forge/miniforge):
+
+    ```shell
+    curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh"
+    bash Mambaforge-$(uname)-$(uname -m).sh
+    ```
+- Create conda/mamba environment and activate it:
+    - Manually:
+
+      ```shell
+      mamba create -n tsp_sim python=3.11
+      mamba activate tsp_sim
+      ```
+    - OR using Pycharm interpreter settings:
+      ![img.png](docs/img.png)
+
+- Install dependencies:
+
+  ```shell
+  ./init.sh --prune
+  ```
+- Have fun! 🥳💃🕺
+
+
+## How to generate TSP dataset?
+
+Type: `python -m mvp.main generate_mvp_dataset --help`
+
+![img_1.png](docs/img_1.png)
+
+Example: 
+
+`python -m mvp.main generate_mvp_dataset --dataset_name=train --seed=368 --size=10 --num_jobs=[10,20]`
+
+or plain argument based
+
+`python -m mvp.main generate_mvp_dataset train 368 10 [10,20]`
+
+will create the following json dataset:
+
+![img.png](docs/img_2.png)
+
+
+
+  
--- mvp/main.py
+++ mvp/main.py
@@ -1,17 +1,18 @@
+from typing import Iterable
+
+import fire
 import tqdm
 
 from app.utils.reproducible import Reproducible
 from mvp.api import State, Sample, Trajectory
 
 
-def generate_expert_trajectory(state: State) -> Trajectory:
+def generate_expert_trajectory(state: State, name: str) -> Trajectory:
     vroom_input = state.get_vroom_input()
     solution = vroom_input.solve()
     samples = list()
     for step in solution.routes[0].job_steps:
-        # logger.debug("state: {}", state)
         job_id = step.id
-        # logger.debug("job_id: {}", job_id)
         samples.append(Sample(
             state=state.model_copy(deep=True),
             action=job_id,
@@ -22,15 +23,22 @@ def generate_expert_trajectory(state: State) -> Trajectory:
         vroom_input=vroom_input,
         solution=solution,
         samples=samples,
+        name=name,
     )
 
 
-def main():
-    rng, seed = Reproducible.get_rng_seed(seed=369)
-    for _ in tqdm.trange(100):
-        state = State.create_initial(num_jobs=rng.integers(10, 20), seed=rng.integers(0, int(1e6)))
-        generate_expert_trajectory(state).save()
+def generate_mvp_dataset(
+        dataset_name: str,
+        seed: int,
+        size: int,
+        num_jobs: Iterable[int],
+
+):
+    rng, seed = Reproducible.get_rng_seed(seed=seed)
+    for _ in tqdm.trange(size):
+        state = State.create_initial(num_jobs=rng.integers(*num_jobs), seed=rng.integers(0, int(1e6)))
+        generate_expert_trajectory(state, name=dataset_name).save()
 
 
 if __name__ == '__main__':
-    main()
+    fire.Fire()
